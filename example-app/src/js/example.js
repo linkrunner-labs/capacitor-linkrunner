12fac01491fe75ed88fa196b4852774b63c9f232
Saksham Sharma <saksham@linkrunner.io>
2026-02-19 09:18:32 +0530

    feat: remove tests and example app

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { Example } from 'capacitor-linkrunner';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    Example.echo({ value: inputValue })
-}
